8d6c00ad175c7f279714994e71102712981ee45c
lPautomaS <riutortp0910@gmail.com>
2026-06-19 10:30:35 +0200

    Add Vercel Speed Insights
    - Install @vercel/speed-insights package
    - Add SpeedInsights component to _app.js
    - Build and tests pass (97 tests)

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import Head from 'next/head';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 class ErrorBoundary extends Component {
   constructor(props) {
@@ -103,6 +104,7 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
       </ErrorBoundary>
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
